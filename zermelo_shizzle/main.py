12a0425f9ea1b18067ff2414cdf5163ac8eaefa4
Mike Wiegman Avila <mike12wiegman@gmail.com>
2022-09-27 21:53:19 +0200

    step 1 for using files for token and key

--- zermelo_shizzle/main.py
+++ zermelo_shizzle/main.py
@@ -3,12 +3,44 @@ import calendar
 import datetime
 import time
 
+def is_key():
+    try:
+        f = open("key.txt", "x")
+        f.close()
+        print("succes")
+        key = False
+        return key
+    except:
+        key = True
+        print("not succeed")
+        return key
 # INITIALISATION
-school = "YOUR SCHOOL HERE"
-token = cl.authenticate("YOUR KEY HERE") # gets token
-token = "YOUR TOKEN HERE" # zermelo token
+school = "maartenscollege"
 cl = zermelo.Client(school) # defines client
+if is_key(): # if there is no key present ask for key
+    pass
+else:
+    key = input('your key') # TODO check if key is aviable if not ask for key
+    f = open("key.txt", "w")
+    f.write(key)
+
+try:
+    f = open("key.txt", "r")
+    print("succees")
+except:
+    print("not succeed")
+    pass
+key = f.readline()
+token = cl.authenticate(key) # gets token
+f.close()
+f = open("zermelo_shizzle/token.txt", "w")
+f.write(str(token))
+print(token)
+f = open("zermelo_shizzle/token.txt", "r") # TODO fix dit stukje en kijk naar deze website: https://www.w3schools.com/python/python_dictionaries_access.asp en token.txt hoe
+token = f.readline()
+token = token
 user = cl.get_user(token) # defines user
+token.close() # closes token file
 ctime = int(time.time())
 yesterday = ctime - 96400
 

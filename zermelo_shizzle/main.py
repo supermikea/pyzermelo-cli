2d7c459751584b5d964604c063090fc3b8f7ab6f
supermikea <mike12wiegman@gmail.com>
2022-10-04 20:04:01 +0200

    added teh beggining of a day as start

--- zermelo_shizzle/main.py
+++ zermelo_shizzle/main.py
@@ -3,51 +3,31 @@ import calendar
 import datetime
 import time
 
-def is_key():
-    try:
-        f = open("key.txt", "x")
-        f.close()
-        print("succes")
-        key = False
-        return key
-    except:
-        key = True
-        print("not succeed")
-        return key
-# INITIALISATION
 school = "maartenscollege"
 cl = zermelo.Client(school) # defines client
-if is_key(): # if there is no key present ask for key
-    pass
-else:
-    key = input('your key') # TODO check if key is aviable if not ask for key
-    f = open("key.txt", "w")
-    f.write(key)
 
-try:
-    f = open("key.txt", "r")
-    print("succees")
-except:
-    print("not succeed")
-    pass
-key = f.readline()
-token = cl.authenticate(key) # gets token
-f.close()
-f = open("zermelo_shizzle/token.txt", "w")
-f.write(str(token))
-print(token)
-f = open("zermelo_shizzle/token.txt", "r") # TODO fix dit stukje en kijk naar deze website: https://www.w3schools.com/python/python_dictionaries_access.asp en token.txt hoe
-token = f.readline()
-token = token
-user = cl.get_user(token) # defines user
-token.close() # closes token file
-ctime = int(time.time())
-yesterday = ctime - 96400
+def authenticate(key):
+    user_info = dict();
+    token = cl.authenticate(key)
+    print(token)
+    user = cl.get_user(token.get('access_token'))
+    print(user)
+    user_info['token'] = token.get('access_token')
+    user_info['user'] = user
+    return user_info
 
-Happointments = cl.get_appointments(token, yesterday, ctime) # appointments kijgen
+def get_shedule(user_info):
 
-Happointments = Happointments.get('response')   # MIKE heeft DE LIST IN DEZE DICTIONARY ERUITgeHAaLd
-appointments = Happointments.get('data') # deze python code zorgt ervoor dat hij de vakkken eruithaalt
-for appointment in appointments: # met deze
-    print(appointment.get('subjects'), end='')
-    print(appointment.get('teachers'))
+
+    ctime = int(time.time())
+    today00 = (ctime - ctime % 86400) - 7200
+
+    Happointments = cl.get_appointments(user_info.get('token'), today00, today00 + 86400) # appointments kijgen
+
+    Happointments = Happointments.get('response')   # MIKE heeft DE LIST IN DEZE DICTIONARY ERUITgeHAaLd
+    appointments = Happointments.get('data') # deze python code zorgt ervoor dat hij de vakkken eruithaalt
+    for appointment in appointments: # met deze
+        print(appointment.get('subjects'), end='')
+        print(appointment.get('teachers'))
+
+get_shedule(authenticate(input("key: ")))

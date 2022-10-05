66f07f6f4358f03c41e0b73fb99f10bcc9802dc6
supermikea <mike12wiegman@gmail.com>
2022-10-05 10:56:53 +0200

    added persistent storage in token file

--- zermelo_shizzle/main.py
+++ zermelo_shizzle/main.py
@@ -2,19 +2,29 @@ import zermelo
 import calendar
 import datetime
 import time
+from os import listdir
 
 school = "maartenscollege"
 cl = zermelo.Client(school) # defines client
 
-def authenticate(key):
+def authenticate():
     user_info = dict();
-    token = cl.authenticate(key)
-    print(token)
-    user = cl.get_user(token.get('access_token'))
-    print(user)
-    user_info['token'] = token.get('access_token')
-    user_info['user'] = user
-    return user_info
+    try:
+        token = write_read_tk(False, 0)
+        user_info['token'] = token
+        user_info['user'] = cl.get_user(token)
+        return user_info
+    except:
+        key = input("key: ")
+        token = cl.authenticate(key)
+        print(token)
+        user = cl.get_user(token.get('access_token'))
+        print(user)
+        user_info['token'] = token.get('access_token')
+        write_read_tk(True, user_info.get('token'))
+        user_info['user'] = user
+        return user_info
+        
 
 def get_shedule(user_info):
 
@@ -30,4 +40,18 @@ def get_shedule(user_info):
         print(appointment.get('subjects'), end='')
         print(appointment.get('teachers'))
 
-get_shedule(authenticate(input("key: ")))
+
+def write_read_tk(option, token): # write or read token from token file
+    if option:
+        file = open("zermelo_shizzle/token", "w")
+        print(token)
+        file.write(token)
+        file.close()
+        return 0
+    else:
+        file = open("zermelo_shizzle/token", "r")
+        token = file.read()
+        file.close()
+        return token
+
+get_shedule(authenticate())

2e1613235259a3dc18a580b64ab01e7e142097df
supermikea <mike12wiegman@gmail.com>
2022-11-10 20:33:41 +0100

    added docstrings to functions

--- zermelo_shizzle/main.py
+++ zermelo_shizzle/main.py
@@ -1,13 +1,14 @@
-import time
-
 import functions
 
 
 def main():  # TODO the actual client
-    print("to be completed")
-    print("for now u will just get the usual prompt")
-    time.sleep(1)
-    functions.get_shedule(functions.authenticate())
+	"""
+	this initializes everything
+	"""
+	print("to be completed")
+	print("for now u will just get the usual prompt")
+	time.sleep(1)
+	functions.get_shedule(functions.authenticate())
 
 
 if __name__ == "__main__":
